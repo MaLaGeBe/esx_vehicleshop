e71a44f7955a5516928e926f86c306a0cadb602e
麻辣戈壁 <lpj.vip@foxmail.com>
2019-03-14 17:19:55 +0800

    Update cn.lua

--- locales/cn.lua
+++ locales/cn.lua
@@ -1,7 +1,7 @@
 Locales['cn'] = {
   --Global menus
   ['vehicle_belongs']         = '牌照为~y~%s~s~的汽车现在属与~b~你~s~了',
-  ['buy_vehicle_shop']        = '你想购买~r~%s~s~吗？需花费~g~$%s。',
+  ['buy_vehicle_shop']        = '你想购买 %s 吗？需花费 $%s。',
   ['car_dealer']              = '西米恩车行',
   ['not_this_car']            = '抱歉，我们现在不回收这辆车',
   ['shop_awaiting_model']     = '载具正在~g~下载&加载中~s~',
